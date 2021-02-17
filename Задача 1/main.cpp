2fcae68e2bfd0fdd124583b6c3cccb9a6105921d
Ivan-Grudev <vanek-stark@yandex.ru>
2021-02-17 20:36:33 +0300

    подключил библиотеку вектор; завёл вектор v на 10 элементов; добавил вывод размера и ёмкости

---
+++ Задача 1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v(10);
+    cout << "vector size is " << v.size() << endl <<
+    "vector capacity is " << v.capacity() << endl;
+    return 0;
+}
