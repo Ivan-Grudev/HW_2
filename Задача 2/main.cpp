4b84f1ff06ca655602b4c5d2d99932acaec8f70c
Ivan-Grudev <vanek-stark@yandex.ru>
2021-02-17 23:06:05 +0300

    создал и проинициализировал 5 контейнеров, заполнив их одинаковым набором элементов

--- Задача 2/main.cpp
+++ Задача 2/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main()
 {
-    //int a[6] = {3, 8, 1, 6, 100, 0};
+    int a[6] = {3, 8, 1, 6, 100, 0};
     list <int> l = {3, 8, 1, 6, 100, 0};
     vector <int> v = {3, 8, 1, 6, 100, 0};
     deque <int> d = {3, 8, 1, 6, 100, 0};
