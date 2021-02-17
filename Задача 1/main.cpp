dcdaba80c22e00fe9f9d0286907f66611760d1ed
Ivan-Grudev <vanek-stark@yandex.ru>
2021-02-17 22:22:47 +0300

    создал вектор размера и ёмкости 3. в цикле добавлял по одному элементу к вектору. когда количество элементов превышало максимальное, выделялась дополнительная память, но не под один новый элемент: ёмкость увеличивалась вдвое. аналогично происходило после использования метода reserve: когда количество элементов превышало выделенную ёмкость, она увеличивалась вдвое от предыдущей. в крайнем случае, когда запрошен СЛИШКОМ большой объём памяти, у компьютера кончается оперативная память и программа вылетает.

--- Задача 1/main.cpp
+++ Задача 1/main.cpp
@@ -5,12 +5,33 @@ using namespace std;
 
 int main()
 {
-    vector<int> v(10);
-    cout << "vector size is " << v.size() << endl <<
-    "vector capacity is " << v.capacity() << endl;
+    vector<int> v(3);
 
-    v.resize(999999999999);
-    cout << "vector capacity now is " << v.capacity() << endl;
+    v = {1,2,3};
+    cout << "vector size is " << v.size() << " vector capacity is " << v.capacity() << endl;
+
+    for (int i = 1; i < 10; i++) {
+        v.push_back(9);
+        cout << "i=" << i << ": vector size is " << v.size() << " vector capacity is " << v.capacity() << endl;
+    }
+
+    //очистка
+    v.clear();
+    cout << endl << "vector size is " << v.size() << " vector capacity is " << v.capacity() << endl;
+    v.shrink_to_fit();
+    cout << "vector size is " << v.size() << " vector capacity is " << v.capacity() << endl;
+
+    v.resize(5);
+    cout << "vector size is " << v.size() << " vector capacity is " << v.capacity() << endl << endl;
+
+    v = {4,5,6};
+    cout << "vector size is " << v.size() << " vector capacity is " << v.capacity() << endl;
+
+    for (int i = 1; i < 10; i++) {
+        v.push_back(9);
+        cout << "i=" << i << ": vector size is " << v.size() << " vector capacity is " << v.capacity() << endl;
+    }
+
 
     return 0;
 }
