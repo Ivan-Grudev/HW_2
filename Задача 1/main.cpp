d4320fde367d1a67169d8a0bb2b8dc500c20c134
Ivan-Grudev <vanek-stark@yandex.ru>
2021-02-17 20:40:44 +0300

    сделал resize вектора v и посмотрел как будет меняться ёмкость. была выведена ошибка bad_alloc

--- Задача 1/main.cpp
+++ Задача 1/main.cpp
@@ -8,5 +8,9 @@ int main()
     vector<int> v(10);
     cout << "vector size is " << v.size() << endl <<
     "vector capacity is " << v.capacity() << endl;
+
+    v.resize(999999999999);
+    cout << "vector capacity now is " << v.capacity() << endl;
+
     return 0;
 }
