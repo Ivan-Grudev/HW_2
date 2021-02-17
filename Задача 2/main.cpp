8f1553be8e64b1e8e937915d96302f44f4cf0932
Ivan-Grudev <vanek-stark@yandex.ru>
2021-02-17 23:04:34 +0300

    подключил все необходимые библиотеки

---
+++ Задача 2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <deque>
+#include <list>
+#include <chrono>
+
+using namespace std;
+
+int main()
+{
+    //int a[6] = {3, 8, 1, 6, 100, 0};
+    list <int> l = {3, 8, 1, 6, 100, 0};
+    vector <int> v = {3, 8, 1, 6, 100, 0};
+    deque <int> d = {3, 8, 1, 6, 100, 0};
+    auto t = make_tuple(3, 8, 1, 6, 100, 0);
+
+    return 0;
+}
